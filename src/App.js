9a02735a850121a3dac7e021b8b3d0dfd8551d6c
Mohamed Aissa Benfodda <rygaa@protonmail.com>
2021-10-27 15:23:16 -0500

    trying to fix isConnected bug 1.5

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
   useEffect(() => {
     setTest(test + 1)
     console.log(test);
-    if (test == 1) {
+    if (test == 4) {
       socket.emit('connection', 0)
       console.log('connection emitted');
 
