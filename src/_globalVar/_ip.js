febc3b5bc79d3b4b650169f1433dd058d0641522
Rygaa <rygaa@protonmail.com>
2022-03-07 03:14:22 -0500

    small changes

--- src/_globalVar/_ip.js
+++ src/_globalVar/_ip.js
@@ -1,3 +1,3 @@
 const local = false;
-const url = local ? `http://192.168.1.4:1005` : 'api.hanasu.me'
+const url = local ? `http://192.168.1.4:1005` : 'https://api.hanasu.me'
 module.exports.url = url
